37aa542d88cad4d944447b8b58946e642410d914
Grevendev <edvin.lindborg@hotmail.se>
2026-09-11 15:03:26 +0200

    update: App.tsx so it can handle /about

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
 import LandingPage from "./pages/LandingPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
+  const path = window.location.pathname;
+
+  if (path === "/about") {
+    return <AboutPage />;
+  }
+
   return <LandingPage />;
 }
 
