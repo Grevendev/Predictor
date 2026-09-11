615dfea320533cc6a510934c5c609471734959de
Grevendev <edvin.lindborg@hotmail.se>
2026-09-11 10:17:24 +0200

    Simple Header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <div>
+        <a href="/">
+          LOGOTYP
+        </a>
+      </div>
+
+      <nav>
+        <a href="/">Hem</a>
+        <a href="/about">Om projektet</a>
+        <a href="/how-it-works">Så fungerar det</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
