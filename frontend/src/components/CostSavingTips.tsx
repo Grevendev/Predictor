aabb122481e47ce80baba32c133fefcccdbcd825
Grevendev <edvin.lindborg@hotmail.se>
2026-09-11 10:34:45 +0200

    Created: this is only info for user. And it doesn´t care where user is

---
+++ frontend/src/components/CostSavingTips.tsx
@@ -0,0 +1,30 @@
+function CostSavingTips() {
+  return (
+    <div>
+      <h3>Tips för att minska elkostnader</h3>
+
+      <ul>
+        <li>
+          Kör tvättmaskin och diskmaskin när elpriset
+          förväntas vara lägre.
+        </li>
+
+        <li>
+          Ladda elbilen under billigare timmar.
+        </li>
+
+        <li>
+          Undvik att använda flera stora elförbrukare
+          samtidigt under dyra timmar.
+        </li>
+
+        <li>
+          Använd prediktionen för att planera din
+          elanvändning i förväg.
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default CostSavingTips;
