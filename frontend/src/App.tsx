dd3955696d3bb9af820711b6acdf96e00ac46ac5
Grevendev <edvin.lindborg@hotmail.se>
2026-09-15 09:41:21 +0200

    feat(frontend): add how it works route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import LandingPage from "./pages/LandingPage";
 import AboutPage from "./pages/AboutPage";
+import HowItWorksPage from "./pages/HowItWorksPage";
 
 function App() {
   const path = window.location.pathname;
 
   if (path === "/about") {
     return <AboutPage />;
+  }
+
+  if (path === "/how-it-works") {
+    return <HowItWorksPage />;
   }
 
   return <LandingPage />;
