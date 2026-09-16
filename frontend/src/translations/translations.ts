c561ae4dfde364fca7ed945651ba36a782fd65c0
Grevendev <edvin.lindborg@hotmail.se>
2026-09-16 13:17:15 +0200

    feat: added first mvp for translations

---
+++ frontend/src/translations/translations.ts
@@ -0,0 +1,41 @@
+export const translations = {
+  sv: {
+    navigation: {
+      home: "Hem",
+      howItWorks: "Så fungerar det",
+      about: "Om oss"
+    },
+
+    hero: {
+      title: "Vill du veta när de är billigast att använda elen i ditt elområde?",
+      cityPlaceholder: "STAD",
+      search: "Sök",
+      loading: "Hämtar information..."
+    },
+
+    howItWorks: {
+      label: "SÅ FUNGERAR DET",
+      title: "Från din stad till en prognos."
+    }
+  },
+
+  en: {
+    navigation: {
+      home: "Home",
+      howItWorks: "How it works",
+      about: "About"
+    },
+
+    hero: {
+      title: "Want to know when electricity is cheapest to use in your area?",
+      cityPlaceholder: "CITY",
+      search: "Search",
+      loading: "Fetching information..."
+    },
+
+    howItWorks: {
+      label: "HOW IT WORKS",
+      title: "From your city to a forecast."
+    }
+  }
+} as const;
